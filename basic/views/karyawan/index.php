d7712e7efef40ef4c9f32f63b2245aef1c8b9789
aliefhd <aliefhd@gmail.com>
2016-06-17 16:38:04 +0700

    Add Octicon icons
    web/css/octicons/

--- basic/views/karyawan/index.php
+++ basic/views/karyawan/index.php
@@ -9,7 +9,10 @@
 </p>
 <div class="container menu">
     <div class="row">
-        <div class="col-xs-3 menubox">a</div>
+        <div class="col-xs-3 menubox">
+        <span class="mega-octicon octicon-flame"></span>
+        <p>asdf</p>
+        </div>
         <div class="col-xs-3 menubox">b</div>
         <div class="col-xs-3 menubox">c</div>
         <div class="col-xs-3 menubox">d</div>
