7bbb63442a89dc5af8740218f3794805ca5b5a9a
aliefhd <aliefhd@gmail.com>
2016-06-23 11:57:50 +0700

    sidebar

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -6,41 +6,58 @@ use yii\web\View;
 $this->title = 'denbeerp';
 ?>
 
+<!-- Sidebars -->
+<!-- General -->
+<div class="sidebar list-group" id="sidebar-general">
+    <button type="button" class="list-group-item">Cras justo odio</button>
+    <button type="button" class="list-group-item">Dapibus ac facilisis in</button>
+    <button type="button" class="list-group-item">Morbi leo risus</button>
+    <button type="button" class="list-group-item">Porta ac consectetur ac</button>
+    <button type="button" class="list-group-item">Vestibulum at eros</button>
+</div>
+<!-- HR -->
+<!-- Procurement -->
+<!-- Marketing -->
+<!-- Fixed Asset -->
+<!-- GL -->
+
+<!-- Content -->
+
 <div class="site-index">
-    <div class="container">
+    <div class="row">
         <div class="col-xs-2">
             <div class="module">
-                <div class="module-icon"><span class="fa fa-tasks fa-5x"></span></div>
+                <div class="module-icon"><span class="fa fa-tasks fa-3x"></span></div>
                 <div class="module-text">General</div>
             </div>
         </div>
         <div class="col-xs-2">
             <div class="module">
-                <div class="module-icon"><span class="fa fa-user fa-5x"></span></div>
+                <div class="module-icon"><span class="fa fa-user fa-3x"></span></div>
                 <div class="module-text">HR</div>
             </div>
         </div>
         <div class="col-xs-2">  
             <div class="module">
-                <div class="module-icon"><span class="fa fa-credit-card-alt fa-5x"></span></div>
+                <div class="module-icon"><span class="fa fa-credit-card-alt fa-3x"></span></div>
                 <div class="module-text">Procurement</div>
             </div>
         </div>
         <div class="col-xs-2">
             <div class="module">
-                <div class="module-icon"><span class="fa fa-line-chart fa-5x"></span></div>
+                <div class="module-icon"><span class="fa fa-line-chart fa-3x"></span></div>
                 <div class="module-text">Marketing</div>
             </div>
         </div>
         <div class="col-xs-2">
             <div class="module">
-                <div class="module-icon"><span class="fa fa-archive fa-5x"></span></div>
+                <div class="module-icon"><span class="fa fa-archive fa-3x"></span></div>
                 <div class="module-text">Fixed Asset</div>
             </div>
         </div>
         <div class="col-xs-2">
             <div class="module">
-                <div class="module-icon"><span class="fa fa-book fa-5x"></span></div>
+                <div class="module-icon"><span class="fa fa-book fa-3x"></span></div>
                 <div class="module-text">GL</div>
             </div>
         </div>
