0217425e9560769546ed15263a31e03b7fef5fc3
aliefhd <aliefhd@gmail.com>
2016-06-23 13:45:02 +0700

    sidebar toggle
    sidebar yang muncul belum bisa sesuai sama button yang di klik

--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -56,12 +56,10 @@ AppAsset::register($this);
     NavBar::end();
     ?>
 
-    <div class="container">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= $content ?>
-    </div>
 </div>
 
 <footer class="footer">
--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -6,60 +6,134 @@ use yii\web\View;
 $this->title = 'denbeerp';
 ?>
 
+<div id="wrapper">
 <!-- Sidebars -->
 <!-- General -->
-<div class="sidebar list-group" id="sidebar-general">
-    <button type="button" class="list-group-item">Cras justo odio</button>
-    <button type="button" class="list-group-item">Dapibus ac facilisis in</button>
-    <button type="button" class="list-group-item">Morbi leo risus</button>
-    <button type="button" class="list-group-item">Porta ac consectetur ac</button>
-    <button type="button" class="list-group-item">Vestibulum at eros</button>
-</div>
-<!-- HR -->
-<!-- Procurement -->
-<!-- Marketing -->
-<!-- Fixed Asset -->
-<!-- GL -->
+    <div class="sidebar" id="sidebar-general">
+        <ul class="list-group">
+            <li class="list-group-item">GENERAL</li>
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+    </div>
+    <!-- HR -->
+    <div class="sidebar" id="sidebar-hr">
+        <ul class="list-group">
+            <li class="list-group-item">HR</li>
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+    </div>
+    <!-- Procurement -->
+    <div class="sidebar" id="sidebar-procurement">
+        <ul class="list-group">
+            <li class="list-group-item">PROCUREMENT</li>
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+    </div>
+    <!-- Marketing -->
+    <div class="sidebar" id="sidebar-marketing">
+        <ul class="list-group">
+            <li class="list-group-item">MARKETING</li>
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+    </div>
+    <!-- Fixed Asset -->
+    <div class="sidebar" id="sidebar-fixedasset">
+        <ul class="list-group">
+            <li class="list-group-item">FIXEDASSET</li>
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+    </div>
+    <!-- GL -->
+    <div class="sidebar" id="sidebar-gl">
+        <ul class="list-group">
+            <li class="list-group-item">GL</li>
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+    </div>
 
-<!-- Content -->
-
-<div class="site-index">
-    <div class="row">
-        <div class="col-xs-2">
-            <div class="module">
-                <div class="module-icon"><span class="fa fa-tasks fa-3x"></span></div>
-                <div class="module-text">General</div>
+    <!-- Content -->
+    <div class="container">
+        <div class="site-index">
+            <div class="col-xs-2">
+                <a id="general-toggle" href="#">
+                    <div class="module">
+                            <div class="module-icon"><span class="fa fa-tasks fa-3x"></span></div>
+                            <div class="module-text">General</div>
+                    </div>
+                </a>
             </div>
-        </div>
-        <div class="col-xs-2">
-            <div class="module">
-                <div class="module-icon"><span class="fa fa-user fa-3x"></span></div>
-                <div class="module-text">HR</div>
+            <div class="col-xs-2">
+                <a id="hr-toggle" href="#">
+                    <div class="module">
+                        <div class="module-icon"><span class="fa fa-user fa-3x"></span></div>
+                        <div class="module-text">HR</div>
+                    </div>
+                </a>
             </div>
-        </div>
-        <div class="col-xs-2">  
-            <div class="module">
-                <div class="module-icon"><span class="fa fa-credit-card-alt fa-3x"></span></div>
-                <div class="module-text">Procurement</div>
+            <div class="col-xs-2"> 
+                <a id="procurement-toggle" href="#">
+                    <div class="module">
+                        <div class="module-icon"><span class="fa fa-credit-card-alt fa-3x"></span></div>
+                        <div class="module-text">Procurement</div>
+                    </div>
+                </a>
             </div>
-        </div>
-        <div class="col-xs-2">
-            <div class="module">
-                <div class="module-icon"><span class="fa fa-line-chart fa-3x"></span></div>
-                <div class="module-text">Marketing</div>
+            <div class="col-xs-2">
+                <a id="marketing-toggle" href="#">
+                    <div class="module">
+                        <div class="module-icon"><span class="fa fa-line-chart fa-3x"></span></div>
+                        <div class="module-text">Marketing</div>
+                    </div>
+                </a>
             </div>
-        </div>
-        <div class="col-xs-2">
-            <div class="module">
-                <div class="module-icon"><span class="fa fa-archive fa-3x"></span></div>
-                <div class="module-text">Fixed Asset</div>
+            <div class="col-xs-2">
+                <a id="fixed-asset-toggle" href="#">
+                    <div class="module">
+                        <div class="module-icon"><span class="fa fa-archive fa-3x"></span></div>
+                        <div class="module-text">Fixed Asset</div>
+                    </div>
+                </a>
             </div>
-        </div>
-        <div class="col-xs-2">
-            <div class="module">
-                <div class="module-icon"><span class="fa fa-book fa-3x"></span></div>
-                <div class="module-text">GL</div>
+            <div class="col-xs-2">
+                <a id="gl-toggle" href="#">
+                    <div class="module">
+                        <div class="module-icon"><span class="fa fa-book fa-3x"></span></div>
+                        <div class="module-text">GL</div>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
 </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script>
+    $("#general-toggle").click(function(e){
+        e.preventDefault();
+        $("#wrapper").toggleClass("displayed-sidebar");
+    });
+</script>
