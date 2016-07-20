3e424e9f58385d1fb1710a7380459b87f135dfbf
aliefhd <aliefhd@gmail.com>
2016-07-20 18:16:40 +0700

    Fixing CSS karyawan & login

--- basic/views/karyawan/update.php
+++ basic/views/karyawan/update.php
@@ -6,17 +6,18 @@ use yii\helpers\Html;
 /* @var $model app\models\Karyawan */
 
 $this->title = 'Update Karyawan: ' . $model->nik;
-$this->params['breadcrumbs'][] = ['label' => 'Karyawans', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->nik, 'url' => ['view', 'id' => $model->nik]];
+$this->params['breadcrumbs'][] = ['label' => 'Karyawan', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => 'View Karyawan '.$model->nik, 'url' => ['view', 'id' => $model->nik]];
 $this->params['breadcrumbs'][] = 'Update';
 $this->params['item']='karyawan';
 ?>
 <div class="karyawan-update">
+    <div class="box">
+        <h1><?= Html::encode($this->title) ?></h1>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+        <?= $this->render('_form', [
+            'model' => $model,
+        ]) ?>    
+    </div>
 
 </div>
--- basic/views/karyawan/view.php
+++ basic/views/karyawan/view.php
@@ -6,14 +6,44 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Karyawan */
 
-$this->title = $model->nik;
-$this->params['breadcrumbs'][] = ['label' => 'Karyawans', 'url' => ['index']];
+$this->title = 'View Karyawan '.$model->nik;
+$this->params['breadcrumbs'][] = ['label' => 'Karyawan', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 $this->params['item']='karyawan';
 ?>
 <div class="karyawan-view">
-
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="box">
+        <h1><?= Html::encode($this->title) ?></h1>
+        <?= DetailView::widget([
+            'model' => $model,
+            'attributes' => [
+                'nik',
+                'nama',
+                'password',
+                'tanggal_lahir',
+                'tempat_lahir',
+                'no_identitas',
+                'jenis_kelamin',
+                'email:email',  
+                'status_pernikahan',
+                'tanggal_masuk',
+                'file_ktp',
+                'file_npwp',
+                'file_bpjs',
+                'file_cv',
+                'file_ijazah',
+                'file_transkrip',
+                'status_karyawan',
+                'sisa_cuti',
+                'jabatan',
+                'gaji',
+                'no_lokasi',
+            ],
+            'options' => [
+                'class' => 'table detail-view'
+            ]
+        ]) ?>
+    </div>
 
     <p>
         <?= Html::a('Update', ['update', 'id' => $model->nik], ['class' => 'btn btn-primary']) ?>
@@ -25,32 +55,5 @@ $this->params['item']='karyawan';
             ],
         ]) ?>
     </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'nik',
-            'nama',
-            'password',
-            'tanggal_lahir',
-            'tempat_lahir',
-            'no_identitas',
-            'jenis_kelamin',
-            'email:email',
-            'status_pernikahan',
-            'tanggal_masuk',
-            'file_ktp',
-            'file_npwp',
-            'file_bpjs',
-            'file_cv',
-            'file_ijazah',
-            'file_transkrip',
-            'status_karyawan',
-            'sisa_cuti',
-            'jabatan',
-            'gaji',
-            'no_lokasi',
-        ],
-    ]) ?>
 
 </div>
